3b70ee7ec72a929fa515a085c81cfb5e66a3c0ab
Atte Heino <atte.heino@gmail.com>
2014-02-08 13:10:25 +0200

    Setting button for difficulty selection

--- puzzle/src/main/java/fi/atteheino/puzzle/MainActivity.java
+++ puzzle/src/main/java/fi/atteheino/puzzle/MainActivity.java
@@ -6,19 +6,43 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
     private static final String TAG = "MainActivity";
-    private String difficulty = "easy";
+    private String difficulty = "supereasy";
     private int[] images;
     private Intent intent;
-
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = (Button) findViewById(R.id.difficultyButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (difficulty.equals("supereasy")) {
+                    difficulty = "easy";
+                    button.setText(org.worldsproject.puzzle.R.string.easy);
+                } else if (difficulty.equals("easy")) {
+                    difficulty = "medium";
+                    button.setText(org.worldsproject.puzzle.R.string.medium);
+                } else if (difficulty.equals("medium")) {
+                    difficulty = "hard";
+                    button.setText(org.worldsproject.puzzle.R.string.hard);
+                } else {
+                    difficulty = "supereasy";
+                    button.setText(org.worldsproject.puzzle.R.string.supereasy);
+                }
+            }
+
+        });
 
 
         Log.v(TAG, "MainActivity OnCreate passed.");
