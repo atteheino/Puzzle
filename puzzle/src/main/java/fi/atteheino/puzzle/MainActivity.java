8479aaea6d8d874c27d548e57fd3489f28cc60de
atteheino <atte.heino@gmail.com>
2014-03-06 22:21:57 +0200

    Adjusting menus and switched from AppCompat to default Activity support

--- puzzle/src/main/java/fi/atteheino/puzzle/MainActivity.java
+++ puzzle/src/main/java/fi/atteheino/puzzle/MainActivity.java
@@ -1,8 +1,8 @@
 package fi.atteheino.puzzle;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,9 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import org.worldsproject.puzzle.PuzzleSolveActivity;
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
     private String difficulty = "supereasy";
@@ -30,7 +28,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        intent = new Intent(this, PuzzleSolveActivity.class);
+        intent = new Intent(this, SolvePuzzleActivity.class);
         firstImage = (ImageView) findViewById(R.id.firstPuzzleImage);
         firstImage.setOnClickListener(new View.OnClickListener() {
             @Override
